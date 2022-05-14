32b2f68cb646627d9c3e1cdf58a27aa85a59d9a0
Dean Scarff <dos@scarff.id.au>
2022-05-14 12:25:36 +1000

    Update vivtool formula to build with Swift
    Resolves sandbox issues with xcodebuild + swift.

--- vivtool.rb
+++ vivtool.rb
@@ -7,52 +7,22 @@ class Vivtool < Formula
   desc "Connect to Viiiiva heart rate monitors"
   homepage "https://github.com/p00ya/vivian"
   license "Apache-2.0"
-  head "https://github.com/p00ya/vivian.git"
+  head "https://github.com/p00ya/vivian.git", branch: "main"
   stable do
     url "https://github.com/p00ya/vivian.git",
       using:    :git,
-      tag:      "v0.2.1",
-      revision: "8c81e9256aa2efbc4529ef1ed2c6d7bc905ef306"
+      tag:      "v1.0.0",
+      revision: "74b8bf9ca4be025e1eb93145d35c93edccf4f81d"
   end
 
   depends_on xcode: ["13.2", :build]
 
-  def setup_fake_sandbox_exec
-    # Workaround for sandbox errors: place a dummy sandbox-exec on PATH.
-    # See: https://github.com/Homebrew/discussions/discussions/59
-    build_bin = "#{buildpath}/bin"
-    Dir.mkdir(build_bin)
-
-    script = <<~SH
-      #!/bin/bash
-      # If a trivial sandbox command can run, then use the system sandbox.
-      if /usr/bin/sandbox-exec -p '(version 1)' true ; then
-        exec /usr/bin/sandbox-exec "$@"
-      fi
-      # If a trivial sandbox command fails, we may already be in a sandbox,
-      # so don't initialize a new sandbox.
-      while getopts ":f:n:p:D:" opt ; do : ; done
-      shift $((OPTIND -1))
-      exec "$@"
-    SH
-    File.write("#{build_bin}/sandbox-exec", script, perm: 0755)
-    ENV["PATH"] = "#{build_bin}:#{ENV["PATH"]}"
-  end
-
   def install
-    setup_fake_sandbox_exec
-
-    xcodebuild "-project", "vivian.xcodeproj",
-        "-scheme", "vivtool",
-        "-disableAutomaticPackageResolution",
-        "-configuration", "Release",
-        "clean", "install",
-        "DSTROOT=build",
-        # Must use absolute path for SYMROOT for SPM dependencies to be
-        # visible.
-        "SYMROOT=#{buildpath}/build"
-    bin.install "build/bin/vivtool"
-    man1.install "build/share/man/man1/vivtool.1"
+    chdir "vivtool" do
+      system "swift", "build", "-c", "release", "--disable-sandbox", "--static-swift-stdlib"
+    end
+    bin.install "vivtool/.build/release/vivtool"
+    man1.install "vivtool/vivtool.1"
   end
 
   test do
